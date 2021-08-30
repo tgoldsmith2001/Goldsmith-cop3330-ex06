f3b81de314ed0871f6ba594e01d0086d68b5a900
HP <tgoldsmith01@yahoo.com>
2021-08-29 21:07:01 -0400

    New changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,11 @@
  *  Copyright 2021 Tyler Goldsmith
  */
 import java.util.Scanner;
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
-        Date d= new Date();
         System.out.print("What is your current age? ");
         String Str1=input.nextLine();
         System.out.print("at what age would you like to retire? ");
@@ -16,8 +15,12 @@ public class Main {
         int a= Integer.parseInt(Str1);
         int b= Integer.parseInt(Str2);
         //gets current year starting from 1900
-        int year= 1900+ d.getYear();
-        System.out.println("You have "+(b-a)+" years until you can retire");
-        System.out.println("It's "+year+", so you can retire in "+(year+b));
+        int year= LocalDate.now().getYear();
+        if(a>b)
+            System.out.println("Congratulations you can retire already!");
+        else {
+            System.out.println("You have " + (b - a) + " years until you can retire");
+            System.out.println("It's " + year + ", so you can retire in " + (year + (b - a)));
+        }
     }
 }
